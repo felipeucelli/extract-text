347bbd0e5a594429ce0800274ab79749f1ee6111
felipeucelli <78266261+felipeucelli@users.noreply.github.com>
2021-07-25 16:15:59 -0400

    Added file save function

--- main.py
+++ main.py
@@ -5,27 +5,61 @@ import _thread
 import fitz
 
 
+def disable_btn():
+    global get_file, extract_file
+    get_file.configure(state=tkinter.DISABLED)
+    extract_file.configure(state=tkinter.DISABLED)
+
+
+def enable_btn():
+    global get_file, extract_file
+    get_file.configure(state=tkinter.ACTIVE)
+    extract_file.configure(state=tkinter.ACTIVE)
+
+
+def save_file(extracted_file):
+    global file_path
+    file_path = file_path.split('/')[len(file_path.split('/')) - 1]
+    new_file_name = file_path.split('.')[0]
+    file_name = filedialog.asksaveasfilename(defaultextension='txt',
+                                             initialfile=new_file_name,
+                                             filetypes=(('Text files', '*.txt'), ('All files', '*.*')))
+
+    print(file_name)
+
+    with open(file_name, 'w') as save:
+        save.writelines(extracted_file)
+
+
 def thread_pdf(*args):
+    disable_btn()
     with fitz.open(args[0]) as pdf:
         text = ''
         for page in pdf:
             text += page.getText()
     print(text)
+    save_file(text)
+    enable_btn()
 
 
 def extract_pdf(pdf_file):
     _thread.start_new_thread(thread_pdf, (pdf_file, 0))
 
 
-def thread_extract(*args):
+def thread_image(*args):
+    disable_btn()
     reader = easyocr.Reader(['pt'])
     result_file = reader.readtext(args[0], paragraph=False)
+    text = ''
     for result in result_file:
         print(f'{result[1]}')
+        text += f'{result[1]}\n'
+    save_file(text)
+    enable_btn()
 
 
 def extract_image(image_file):
-    _thread.start_new_thread(thread_extract, (image_file, 0))
+    _thread.start_new_thread(thread_image, (image_file, 0))
 
 
 def extract():
@@ -42,7 +76,7 @@ def extract():
 def get_file_path():
     global file_path
     global label
-    file_path = filedialog.askopenfilename(filetypes=(('PDF files', '*.pdf'),
+    file_path = filedialog.askopenfilename(filetypes=(('Pdf files', '*.pdf'),
                                                       ('Image files', '*.jpg'),
                                                       ('All files', '*.*')))
 
