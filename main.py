ca03c16c35815eddae654daf8cde12861dec3587
felipeucelli <78266261+felipeucelli@users.noreply.github.com>
2021-07-24 23:49:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,39 @@ import tkinter
 from tkinter import filedialog
 
 
+def extract_pdf(pdf_file):
+    import fitz
+
+    with fitz.open(pdf_file) as pdf:
+        texto = ''
+        for pagina in pdf:
+            texto += pagina.getText()
+
+    print(texto)
+
+
+def extract_image(image_file):
+    import easyocr
+
+    reader = easyocr.Reader(['pt'])
+
+    resultados = reader.readtext(image_file, paragraph=False)
+
+    for resultado in resultados:
+        print(f'{resultado[1]}')
+
+
+def extract():
+    global file_path
+    if file_path != '':
+        file = file_path.split('/')[len(file_path.split('/')) - 1]
+
+        if file.split('.')[1] == 'pdf':
+            extract_pdf(file_path)
+        if file.split('.')[1] == 'jpg':
+            extract_image(file_path)
+
+
 def get_file_path():
     global file_path
     global label
@@ -23,5 +56,8 @@ canvas.create_window(150, 60, window=label)
 
 get_file = tkinter.Button(text='    Import File    ', font='Arial 15', command=get_file_path)
 canvas.create_window(150, 130, window=get_file)
+
+extract_file = tkinter.Button(root, text='    Extract File    ', font='Arial 15', command=extract)
+canvas.create_window(150, 180, window=extract_file)
 
 root.mainloop()
